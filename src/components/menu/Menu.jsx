74b334a2042aca151798e8701e3bdb819c11ea41
GabrielaRamos79 <gabyramos@me.com>
2024-10-28 19:39:47 +0100

    Menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,3 +1,4 @@
+import "./Menu.css"
 import fondo from "../../assets/images/menu-img/fondo-menu"
 import spanish from "../../assets/images/español"
 import english from "../../assets/images/ingles"
@@ -6,6 +7,19 @@ import cierre from "../../assets/images/cierre"
 
 function Menu() {
     return (
-        
-    )
+        <>
+        <div className="menu">
+            <img src="fondo" alt="pimientos" />
+        </div>
+        <div className="menu-icons">
+            <img src={spanish} alt="español" />
+            <img src={english} alt="ingles" />
+        </div>
+        <div className="logo-menu">
+            <img src={logo} alt="logo" />
+        </div>
+
+        </>
+
+    );
 }
