1c8e9628428617c6fdc5f60256bd36e3287be408
GabrielaRamos79 <gabyramos@me.com>
2024-10-28 19:25:56 +0100

    Starting with menu

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,11 @@
+import fondo from "../../assets/images/menu-img/fondo-menu"
+import spanish from "../../assets/images/español"
+import english from "../../assets/images/ingles"
+import logo from "../../assets/images/logo-amarillo"
+import cierre from "../../assets/images/cierre"
+
+function Menu() {
+    return (
+        
+    )
+}
