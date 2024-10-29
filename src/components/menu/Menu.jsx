699f0c19145382b0cf8796fc092e952c50284c32
Yuliia <jasmin.ic.km.ua13@gmail.com>
2024-10-29 23:01:27 +0100

    Estructura html del componente menu + css

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,37 @@
+import './Menu.css';
+
+function Menu() {
+return (
+    /*TODO hacer rutas*/
+    <div className="container">
+        <div className="containerList">
+            <div className="item">
+                <p className="text">
+                    PRODUCTOS
+                </p>
+                </div>
+            <div className="item">
+                <p className="text">
+                    LA TRIBU
+                </p> 
+                </div>
+            <div className="item">
+                <p className="text">
+                    RECETAS
+                </p>
+                </div>
+            <div className="item">
+                <p className="text">
+                    CONTACTO
+                </p>
+                </div>
+            <div className="item">
+                <p className="text">
+                    NOTICIAS
+                </p>
+                </div>
+        </div>
+    </div>
+)
+}
+export default Menu;
