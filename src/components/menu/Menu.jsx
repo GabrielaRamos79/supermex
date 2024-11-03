be6c05388ae6f1a4c068ac9f9dd8baa753068978
Yuliia <jasmin.ic.km.ua13@gmail.com>
2024-11-03 21:33:44 +0100

    Refactor navigation logic and component structure, finished component Menu (some things still need to fix): - Modified `main` and `routes` to implement navigation logic - Renamed and updated components in the `pages` folder for navigation testing - Added direct navigation functionality in the `Menu` component - Commented out content in `App.jsx` and `index.css` as they are currently unused

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,37 +1,32 @@
 import './Menu.css';
+import { NavLink } from "react-router-dom"
 
 function Menu() {
-return (
-    /*TODO hacer rutas*/
-    <div className="container">
-        <div className="containerList">
-            <div className="item">
-                <p className="text">
-                    PRODUCTOS
-                </p>
-                </div>
-            <div className="item">
-                <p className="text">
-                    LA TRIBU
-                </p> 
-                </div>
-            <div className="item">
-                <p className="text">
-                    RECETAS
-                </p>
-                </div>
-            <div className="item">
-                <p className="text">
-                    CONTACTO
-                </p>
-                </div>
-            <div className="item">
-                <p className="text">
-                    NOTICIAS
-                </p>
-                </div>
+    return (
+        <div className="container">
+            <div className="containerList">
+ {/*FIXME flujo no tiene info a donde tiene que llevarte "Productos" por ahora dejo a Home*/} 
+                <NavLink to="/" className="item">
+                    <p className="text">PRODUCTOS</p>
+                </NavLink>
+                
+                <NavLink to="/tribu" className="item">
+                    <p className="text">LA TRIBU</p>
+                </NavLink>
+                
+                <NavLink to="/recetas" className="item">
+                    <p className="text">RECETAS</p>
+                </NavLink>
+                
+                <NavLink to="/contacto" className="item">
+                    <p className="text">CONTACTO</p>
+                </NavLink>
+                
+                <NavLink to="/noticias" className="item">
+                    <p className="text">NOTICIAS</p>
+                </NavLink>
+            </div>
         </div>
-    </div>
-)
+    );
 }
 export default Menu;
