fb0312b5824df7fd35e9d3c086358858a8257e18
Yuliia <jasmin.ic.km.ua13@gmail.com>
2024-12-04 20:25:06 +0100

    html: start with TibuAztecas

--- src/components/tribu/tribu-aztecas/TribuAztecas.jsx
+++ src/components/tribu/tribu-aztecas/TribuAztecas.jsx
@@ -1,9 +1,23 @@
 import '../tribu-aztecas/TribuAztecas.css'
 
 const TribuAztecas = () => {
-  
+
   return (
-    <div className='aztecas-container'>Tribu-Aztecas</div>
+    <div className='aztecas-container'>Tribu-Aztecas
+      <section className='left-section'>
+        <img src="" alt="Fire icon" />
+        <div className='img-bottom-holder'>
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+
+      </section>
+      <section className='right-section'>
+        <p>Los antiguos aztecas nos regalaron sabores milenarios que aún disfrutamos hoy en día.</p>
+        <p>El aroma de las tortillas calentitas, el picante sabor del guacamole, el queso fundiéndose con los nachos adornados con chili y jalapeños… son una fiesta para el paladar. Los tiempos han cambiado, pero la esencia y la deliciosa tradición azteca perduran en cada bocado.</p>
+        <p>Y, aunque no sea alrededor de una fogata en la aldea, es con tu tribu con quienes querrás disfrutar de este festín. </p>
+      </section>
+    </div>
   )
 }
 
