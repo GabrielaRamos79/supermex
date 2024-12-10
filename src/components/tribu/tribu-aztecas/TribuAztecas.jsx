7a174be5745bb697895f94946ae91219909001f3
Yuliia <jasmin.ic.km.ua13@gmail.com>
2024-12-10 22:07:50 +0100

    css: modif TribuAztecas + bacis responsive

--- src/components/tribu/tribu-aztecas/TribuAztecas.jsx
+++ src/components/tribu/tribu-aztecas/TribuAztecas.jsx
@@ -1,25 +1,36 @@
 import '../tribu-aztecas/TribuAztecas.css'
 import ImgQuesadillas from '../../../assets/images/tribu-img/quesadillas.png'
 import ImgNachos from '../../../assets/images/tribu-img/nachos.png'
+import IconFire from '../../../assets/images/tribu-img/fire-icon-tribu.png'
+import IconArrow from '../../../assets/images/tribu-img/arrow-icon-tribu.png'
+import ImgSmile from '../../../assets/images/tribu-img/smile-face.png'
+
 const TribuAztecas = () => {
 
   return (
     <div className='aztecas-container'>
       <section className='left section'>
-        <img className='fire-icon' src="" alt="Fire icon" />
         <div className='img-bottom-holder'>
-          <img src={ImgNachos} alt="Foto de los nachos con guacamole" />
-          <img src={ImgQuesadillas} alt="Quesadillas con pollo y queso" />
+          <img className='img'src={ImgNachos} alt="Foto de los nachos con guacamole" />
+          <img className='fire-icon' src={IconFire} alt="Fire icon" />
+          <img className='img'src={ImgQuesadillas} alt="Quesadillas con pollo y queso" />
         </div>
+
 
       </section>
       <section className='right section'>
         <p>Los antiguos aztecas nos regalaron sabores milenarios que aún disfrutamos hoy en día.</p>
-        <p>El aroma de las tortillas calentitas, el picante sabor del guacamole, 
-          el queso fundiéndose con los nachos adornados con chili y jalapeños... son una 
-          fiesta para el paladar. Los tiempos han cambiado, pero la esencia y la deliciosa 
+        <div className='emoji-holder'>
+        <img src={ImgSmile} alt="Emoji smile" />
+        </div>
+        <p>El aroma de las tortillas calentitas, el picante sabor del guacamole,
+          el queso fundiéndose con los nachos adornados con chili y jalapeños... son una
+          fiesta para el paladar. Los tiempos han cambiado, pero la esencia y la deliciosa
           tradición azteca perduran en cada bocado.</p>
         <p>Y, aunque no sea alrededor de una fogata en la aldea, es con tu tribu con quienes querrás disfrutar de este festín. </p>
+        <div class="image-overlay">
+          <img className='arrow-icon' src={IconArrow} alt="Arrow icon" />
+        </div>
       </section>
     </div>
   )
