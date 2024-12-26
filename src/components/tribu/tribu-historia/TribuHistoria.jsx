d884a46127ceb548262728f6ff2a564e591f694e
Yuliia <jasmin.ic.km.ua13@gmail.com>
2024-12-26 17:10:37 +0100

    html: add html structure to TribuHistoria

---
+++ src/components/tribu/tribu-historia/TribuHistoria.jsx
@@ -0,0 +1,49 @@
+import '../tribu-historia/TribuHistoria.css';
+
+const TribuHistoria = () => {
+    return (
+        <div className="history-container">
+            <section className="section top-section">
+                <div className="text-holder">
+                    <img src="" alt="Icono de la estrella rosa" />
+                    <p>¿Conoces nuestr historia?</p>
+                </div>
+
+                <div className="img-holder">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </section>
+
+            <section className="section center-section">
+                <div>
+                    <img src="" alt="Icono de la pequeña estrella rosa" />
+                    <p>En Supermex llevamos desde XXXX compartiendo los mejores productos mexicanos y tex-mex para que triunfes con tus recetas.
+                    </p>
+                </div>
+
+                <div>
+                    <p>Comenzamos esta gran aventura en XXXX de la mano de dos familias americanas que llegaron hasta el Puerto de Santa María, en Cádiz. El producto que comenzamos fabricando y distribuyendo fueron nuestras populares tortilla chips, conocidas comúnmente en España como “nachos”.
+                    </p>
+                </div>
+            </section>
+
+            <section className="section bottom-section">
+                <div>
+                    <img src="" alt="Icono de la estrella amarillo" />
+                    <img src="" alt="Nachos con la salsa de los tomates" />
+                </div>
+                <div>
+                    <p>Pero, nachos es el nombre que lleva el plato formado por tortilla chips, carne picada, jalapeños, queso fundido, pico de gaño...</p>
+                    <p>Ya lo sabías, ¿verdad?</p>
+                    <p>Gracias al delicioso sabor, y la gran calidad de nuestros productos, hemos crecido hasta convertirnos en un proveedor internacional de gran variedad de comida mexicana y tex-mex. </p>
+                    <p>Contamos con el más avanzado sistema de producción y de control de calidad, obteniendo, además, numerosos premios que aplauden nuestro sabor y compromiso con la creación de productos de gran calidad.</p>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default TribuHistoria;
--- src/pages/TribuPage.jsx
+++ src/pages/TribuPage.jsx
@@ -1,11 +1,13 @@
 import TribuAztecas from "../components/tribu/tribu-aztecas/TribuAztecas";
 import TribuHeader from "../components/tribu/tribu-header/TribuHeader";
+import TribuHistoria from "../components/tribu/tribu-historia/TribuHistoria";
 
 function TribuPage(){
     return (
         <>
      <TribuHeader />
      <TribuAztecas />
+     <TribuHistoria />
         </>
     )
 }
