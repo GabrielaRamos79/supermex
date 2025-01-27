7cc0f2aad964292bcdb2d8cd59dc527dfbfbfa3a
GabrielaRamos79 <gabyramos@me.com>
2025-01-27 19:47:35 +0100

    Home header

---
+++ src/components/home/home-header/Home-Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Home-Header.css';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <div className="logo">Logo</div>
+        </header>
+    );
+};
+export default Home-Header;
