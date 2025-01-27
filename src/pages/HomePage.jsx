78297b86b7887d3f05a3b68f771154d52b5c06d2
GabrielaRamos79 <gabyramos@me.com>
2025-01-27 20:08:08 +0100

    home-header beginning

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import NavBar from "../components/navbar/NavBar";
-
+import Home from "../components/home/Home";
 // aqui teneis que importar componente Home
 function HomePage(){
     return (
         <>
         <NavBar/>
-        <p>Hola soy pagina Home</p>
-      {/*<Home /> */}
+        {/* <p>Hola soy pagina Home</p> */}
+      <Home/>
         </>
     )
 }
