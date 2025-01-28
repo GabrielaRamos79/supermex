e2fa83a405b1d09ab3b8fdd0c5449896f8e6064b
GabrielaRamos79 <gabyramos@me.com>
2025-01-28 19:25:49 +0100

    Footer export default

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 SuperMexFoodGroup. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer; // Asegúrate de incluir esta línea
